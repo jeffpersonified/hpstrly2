4512e5d66c64919f14bbd71feccc164dac3ed4e7
jeffpersonified <jeff.matthew.smith@gmail.com>
2013-09-03 13:18:22 -0700

    optimize and homogenize query and increment methods

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -4,7 +4,7 @@ class UrlsController < ApplicationController
   def index
     @url = Url.new
     if current_user
-      @urls = Url.where(user_id: current_user.id)
+      @urls = Url.find_by(user_id: current_user.id)
     elsif session[:url_ids] && session[:url_ids].any?
       @urls = Url.find(session[:url_ids])
     else
@@ -17,7 +17,7 @@ class UrlsController < ApplicationController
   end
 
   def show
-    Url.where(id: @url.id).update_all("page_views = page_views + 1") # Is this all that performant?
+    @url.increment!(:page_views)
     redirect_to @url.original_url
   end
 
@@ -30,7 +30,7 @@ class UrlsController < ApplicationController
         format.html { redirect_to urls_path, notice: 'Url was successfully created.' }
         format.json { render action: 'index', status: :created, location: @url }
       else
-        format.html { render action: 'new' }
+        format.html { render action: 'index' }
         format.json { render json: @url.errors, status: :unprocessable_entity }
       end
     end
@@ -47,13 +47,10 @@ private
   end
 
   def set_by_short_url
-   @url = Url.where(short_url: params[:short_url]) # Where are these params coming from?
+    @url = Url.find_by(short_url: params[:short_url])
   end
 
   def url_params
-    debugger
-    # params[:url].permit(:url)
     params.require(:url).permit(:original_url, :short_url)
-    # params.require(:url).permit!
   end
 end
