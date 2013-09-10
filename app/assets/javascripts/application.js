1ba38467d07e74cdae9dded7ae164bd4321a8920
jeffpersonified <jeff.matthew.smith@gmail.com>
2013-09-09 20:29:12 -0700

    remove angular gem, move to google cdn

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,8 +14,6 @@
 //= require jquery_ujs
 //= require foundation
 //= require turbolinks
-//= require angular
-//= require angular-resource
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
